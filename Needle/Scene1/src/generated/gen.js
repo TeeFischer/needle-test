c2ecfb354952c794b0e060d0bbc302461be8a5bd
Tilman <t.fischer@prijanto-labs.com>
2025-09-04 14:29:48 +0200

    dev build instead of prod build (unzipped)

--- Needle/Scene1/src/generated/gen.js
+++ Needle/Scene1/src/generated/gen.js
@@ -2,13 +2,13 @@
 
 export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("assets/Scene1.glb?v=1756986989900");
+needle_exported_files.push("assets/Scene1.glb?v=1756988779506");
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1756986989900");
+		needleEngine.setAttribute("hash", "1756988779506");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
