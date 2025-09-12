0057876489b4dcd1e8c68f99a9bf2aecbd865560
Tilman <t.fischer@prijanto-labs.com>
2025-09-12 14:27:38 +0200

    fahrrad updatet, xrrig deaktiviert

--- Needle/Scene1/src/generated/gen.js
+++ Needle/Scene1/src/generated/gen.js
@@ -2,13 +2,13 @@
 
 export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("assets/Scene1.glb?v=1757676542599");
+needle_exported_files.push("assets/Scene1.glb?v=1757679683836");
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1757676542599");
+		needleEngine.setAttribute("hash", "1757679683836");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
