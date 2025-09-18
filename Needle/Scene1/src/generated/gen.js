23d7ea9563f1ffaee93f7e15898f41049d6293e1
Tilman <t.fischer@prijanto-labs.com>
2025-09-18 15:48:57 +0200

    schnitt überarbeitet

--- Needle/Scene1/src/generated/gen.js
+++ Needle/Scene1/src/generated/gen.js
@@ -2,13 +2,13 @@
 
 export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("assets/Scene1.glb?v=1758200082005");
+needle_exported_files.push("assets/Scene1.glb?v=1758203173083");
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1758200082005");
+		needleEngine.setAttribute("hash", "1758203173083");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
