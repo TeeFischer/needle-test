ee3bc29a93a491e3e17dd433f0e0ebaa4acde438
Tilman <t.fischer@prijanto-labs.com>
2025-09-19 13:45:30 +0200

    added recall button

--- Needle/Scene1/src/generated/gen.js
+++ Needle/Scene1/src/generated/gen.js
@@ -2,13 +2,13 @@
 
 export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("assets/Scene1.glb?v=1758206078340");
+needle_exported_files.push("assets/Scene1.glb?v=1758280674635");
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1758206078340");
+		needleEngine.setAttribute("hash", "1758280674635");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
