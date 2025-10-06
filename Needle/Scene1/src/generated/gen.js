d1e10fc256c29353894015e52ffd1f89b3360d36
Tilman <t.fischer@prijanto-labs.com>
2025-10-06 15:53:14 +0200

    back to single scene

--- Needle/Scene1/src/generated/gen.js
+++ Needle/Scene1/src/generated/gen.js
@@ -2,13 +2,13 @@
 
 export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("assets/MainScene.glb?v=1759757196974");
+needle_exported_files.push("assets/ObservationScene.glb?v=1759758723853");
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1759757196974");
+		needleEngine.setAttribute("hash", "1759758723853");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
